b98870942fc6ed866849d1c10ea126bb08db8fcb
Anthony Keith <jt1keith@gmail.com>
2023-05-12 11:50:48 -0700

    lab complete

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -3,32 +3,71 @@ import './Form.scss';
 
 function Form(props) {
   const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method: 'GET',
+      method: method,
       url: url,
     };
     props.handleApiCall(formData);
-  }
+  };
+
+  const handleMethodChange = (e) => {
+    setMethod(e.target.value);
+  };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>URL: </span>
-          <input name='url' type='text' value={url} onChange={(e) => setUrl(e.target.value)} />
-          <button type="submit">GO!</button>
-        </label>
-        <label className="methods">
-          <span id="get">GET</span>
-          <span id="post">POST</span>
-          <span id="put">PUT</span>
-          <span id="delete">DELETE</span>
-        </label>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>URL: </span>
+        <input name="url" type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+        <button type="submit">GO!</button>
+      </label>
+      <label className="methods">
+        <span>
+          <input
+            type="radio"
+            name="method"
+            value="GET"
+            checked={method === 'GET'}
+            onChange={handleMethodChange}
+          />
+          GET
+        </span>
+        <span>
+          <input
+            type="radio"
+            name="method"
+            value="POST"
+            checked={method === 'POST'}
+            onChange={handleMethodChange}
+          />
+          POST
+        </span>
+        <span>
+          <input
+            type="radio"
+            name="method"
+            value="PUT"
+            checked={method === 'PUT'}
+            onChange={handleMethodChange}
+          />
+          PUT
+        </span>
+        <span>
+          <input
+            type="radio"
+            name="method"
+            value="DELETE"
+            checked={method === 'DELETE'}
+            onChange={handleMethodChange}
+          />
+          DELETE
+        </span>
+      </label>
+    </form>
   );
 }
 
